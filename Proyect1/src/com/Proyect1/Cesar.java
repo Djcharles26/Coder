0dfc2454f24a7888b9373ae8bd04a6ec36366818
Luis Gerardo Huerta Sánchez <luisgerhuerta@gmail.com>
2019-02-22 13:56:36 -0600

    Mod cesar

--- Proyect1/src/com/Proyect1/Cesar.java
+++ Proyect1/src/com/Proyect1/Cesar.java
@@ -1,5 +1,8 @@
 package com.Proyect1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Cesar extends Cifrar{
     public Cesar (String mensaje){
         super.Cifrar(mensaje);
@@ -9,7 +12,7 @@ public class Cesar extends Cifrar{
         char [] caracteres = mensajeSinCifrar.toCharArray(); // array de mensaje sin cifrar
         char [] abc = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
         char [] abcesar = {'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C'};
-        char [] cesar = new char[1000];
+        char [] mensajeCifrado = new char[1000];
         int x = mensajeSinCifrar.length();
         // recorrer mensaje sin cifrar
         for (int i = 0; i < x; i++){
@@ -17,19 +20,18 @@ public class Cesar extends Cifrar{
             for (int j = 0; j < 26; j++){
                 // checa espacios
                 if (caracteres[i] == ' '){
-                    cesar[i] = ' ';
+                    mensajeCifrado[i] = ' ';
                 }
                 if (caracteres[i] == abc[j]){
-                    cesar[i] = abcesar[i];
+                    mensajeCifrado[i] = abcesar[i];
                     j = 27;
                 }
             }
         }
-        String 
         return null;
     }
     public String getMensajeCifrado(){
-        return mensajeCifrado;
+        return Arrays.toString(mensajeCifrado);
     }
     public void setContraseña (String contraseña){
         Contraseña = "0";
